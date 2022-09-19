f2bf140f0d24aa2974fff4ea4e64059e2aebb27f
robennals <rob@ennals.org>
2022-09-19 15:40:59 -0700

    Missing from previous commit

--- client/components/connectedbanner.js
+++ client/components/connectedbanner.js
@@ -1,1 +1,23 @@
+import React, { useContext, useEffect, useState } from 'react';
+import _ from 'lodash';
+import { Entypo } from '@expo/vector-icons';
+import { SafeAreaView, Text, View } from 'react-native';
+import { NetworkContext } from './context';
+
+export function ConnectedBanner() {
     const {connected, waited} = useContext(NetworkContext);
+    if (!connected && waited) {
+        return (
+            <SafeAreaView>
+                <View style={{backgroundColor: 'red', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', padding: 8}}>
+                    <Entypo name='warning' color='white' size={15} style={{marginTop: 1}} />
+                    <Text style={{color: 'white', fontWeight: 'bold', marginLeft: 8}}>
+                        Network Connection Lost
+                    </Text>
+                </View>
+            </SafeAreaView>
+        )
+    } else {
+        return null;
+    }
+}
