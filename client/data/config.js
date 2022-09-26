a711c5d63276c2980417d7d47e8d77b120e0a77e
robennals <rob@ennals.org>
2022-09-26 15:05:47 -0700

    Version bump - Android fixes

--- client/data/config.js
+++ client/data/config.js
@@ -33,7 +33,7 @@ export const highlightColor = '#ffffdd'
 
 export const minTwoPanelWidth = 700
 
-export const version = 32
+export const version = 33
 
 export const experienceId = '@robennals/iris-talk';
 
