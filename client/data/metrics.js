e699c092dd5d9748537676d64862daf041dff918
robennals <rob@ennals.org>
2022-09-27 17:07:35 -0700

    User facts writes OS name in a prettier way.

--- client/data/metrics.js
+++ client/data/metrics.js
@@ -2,8 +2,17 @@ import { Platform } from "react-native";
 import { people_set } from "../components/shim";
 import * as Notifications from 'expo-notifications';
 import Constants from "expo-constants"
-import { getCurrentUser, getDataAsync } from "./fbutil";
+import { getCurrentUser, getDataAsync, setDataAsync, updateDataAsync } from "./fbutil";
+import { version } from "./config";
 
+function osName() {
+    switch (Platform.OS) {
+        case 'web': return 'Web';
+        case 'ios': return 'iOS';
+        case 'android': return 'Android';
+        default: return 'Unknown'
+    }
+}
 
 export async function setUserPropertiesAsync() {
     var props = {};
@@ -13,17 +22,20 @@ export async function setUserPropertiesAsync() {
     const notifStatus = await Notifications.getPermissionsAsync();
 
     props['Last Platform'] = Platform.OS;
-    props['Has Platform ' + Platform.OS] = true;
+    props['Has Platform ' + osName()] = true;
     props['Mobile Notifs Connected'] = notifToken != null;
     props['Web Notifs Connected'] = webToken != null;
-    props['App Version ' + Platform.OS] = Constants.expoConfig.version;
- 
+    props['App Version ' + osName()] = Constants.expoConfig.version;
+    props['Hotfix ' + osName()] = version;
+
+    
     if (Platform.OS != 'web') {
         props['Mobile Notifs Granted'] = notifStatus == 'granted';
     } else {
         props['Web Notifs Granted'] = notifStatus == 'granted';
     }
     console.log('set user properties', props);
+    updateDataAsync(['perUser', 'props', getCurrentUser()], props);
     await people_set(props);
 }
 
