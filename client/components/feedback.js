3c922b647e73528227b897b069bb11aa8dcba6c8
robennals <rob@ennals.org>
2022-10-03 11:01:38 -0700

    Feedback requires pressing submit button.

--- client/components/feedback.js
+++ client/components/feedback.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import _ from 'lodash';
 import { StyleSheet, Text, View } from 'react-native';
-import { FixedTouchable, FormInput } from './basics';
+import { FixedTouchable, FormInput, WideButton } from './basics';
 import { Entypo } from '@expo/vector-icons';
 import { getCurrentUser, setDataAsync, useDatabase } from '../data/fbutil';
 import { getCurrentDomain } from './shim';
+import { LinkText } from './linktext';
 
 const shadowStyle = {
     shadowRadius: 4, shadowColor: '#555', shadowOffset: {width: 0, height: 2},
@@ -35,19 +36,26 @@ export function Feedback({archived, group}) {
     if (!archived) return null;
     const rating = useDatabase([group], ['perUser','groupRating',getCurrentUser(), group], 0);
     const extraText = useDatabase([group], ['perUser', 'groupFeedback', getCurrentUser(), group], '');
+    const [localText, setLocalText] = useState(null);
+    const [inProgress, setInProgress] = useState(false);
+    const [submitted, setSubmitted] = useState(null);
 
     function onChooseRating(rating) {
         setDataAsync(['perUser', 'groupRating', getCurrentUser(), group], rating);
         setDataAsync(['userPrivate', getCurrentUser(), 'group', group, 'rating'], rating);
     }
 
-    function setExtraText(extraText) {
-        setDataAsync(['perUser', 'groupFeedback', getCurrentUser(), group], extraText);
-
+    async function setExtraText(newText) {
+        setInProgress(true);
+        setSubmitted(extraText ? 'Updated' : 'Submitted');
+        await setDataAsync(['perUser', 'groupFeedback', getCurrentUser(), group], newText);
+        setLocalText(null);
+        setInProgress(false);
     }
 
-    const expanded = extraText && extraText != '';
-
+    const mergedText = localText == null ? extraText : localText;
+    const expanded = mergedText && mergedText != '';
+    
     return (
         <View style={{alignSelf: 'center', maxWidth: 550, marginTop: 32}}>
             <View style={{borderWidth: StyleSheet.hairlineWidth, borderColor: '#ddd', borderRadius: 8,
@@ -58,9 +66,19 @@ export function Feedback({archived, group}) {
                 <Text style={{fontWeight: 'bold', marginBottom: 4}}>Rate this Conversation</Text>
                 <StarRating rating={rating} onChooseRating={onChooseRating} />
 
-                <FormInput multiline value={extraText || ''} onChangeText={setExtraText}
+                <FormInput multiline value={mergedText || ''} onChangeText={setLocalText}
                     extraStyle={{marginTop: 16, minWidth: 200, marginHorizontal: 8, flex: null, height: expanded ? 200 : null, alignSelf: 'stretch' }} 
                     placeholder='Other Feedback (optional)'/>
+
+                {submitted && !localText ? 
+                    <Text style={{fontSize: 12, color: '#666'}}>{submitted}</Text>
+                :null}
+                {localText != null ? 
+                    <WideButton style={{alignSelf:'flex-start', margin: 4}} onPress={() => setExtraText(localText)} alwaysActive 
+                        disabled={inProgress} progressText={extraText ? 'Updating...' : 'Submitting...'}>
+                        {extraText ? 'Update' : 'Submit'}
+                    </WideButton>
+                : null }
             </View>
             <Text style={{color: '#666', fontSize: 12, maxWidth: 250, marginHorizontal: 32, textAlign: 'center'}}>
                 Rating this conversation will help us match you with better conversations.
