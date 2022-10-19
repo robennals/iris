7fa890fc228183248a28b659eeaa5ae4c098eddb
robennals <rob@ennals.org>
2022-10-18 21:26:36 -0700

    Version bump

--- client/data/config.js
+++ client/data/config.js
@@ -33,7 +33,7 @@ export const highlightColor = '#ffffdd'
 
 export const minTwoPanelWidth = 700
 
-export const version = 38
+export const version = 39
 
 export const experienceId = '@robennals/iris-talk';
 
